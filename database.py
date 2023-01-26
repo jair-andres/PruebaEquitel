0b74331d1c606752f8c77888ef4e9e90f46bcd7d
Jair Bareño <jairbarreto23@gmail.com>
2023-01-25 23:10:12 -0500

    ultimos cambios

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import certifi
 
-MONGO_URI = 'mongodb+srv://JAIR787:Gatos787@cluster0.ibtchgl.mongodb.net/test'
+MONGO_URI = 'mongodb+srv://JAIR787:password@cluster0.ibtchgl.mongodb.net/test'
 ca = certifi.where()
 
 def dbConnection():
